7e39dc747d831335fcc9d2ec967659e9df50207e
hyerim <hyerimiya1216@gmail.com>
2023-03-02 22:07:34 +0900

    Feat : 이벤트 설치하기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,9 +7,10 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    // this.state = {} // state값 초기화
     this.state = {
+      mode: 'welcome',
       subject: { title: "WEB", sub: "World Wid Web!" },
+      welcome: {title: 'Welcome', desc: 'Hello, React!!!'}, // mode 가 welcome일 경우의 state 설정
       contents: [
         { id: 1, title: "HTML", desc: "HTML is for information" },
         { id: 2, title: "CSS", desc: "CSS is for design" },
@@ -19,6 +20,16 @@ class App extends Component {
   }
 
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App">
         <Subject
@@ -27,8 +38,8 @@ class App extends Component {
         ></Subject>
         <TOC data={this.state.contents}></TOC>
         <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
+          title={_title}
+          desc={_desc}
         ></Content>
       </div>
     );
